c25b748819e9961ff1508b2af2ba9af522811e33
LGiki <qq547221432@gmail.com>
2019-06-17 15:53:43 +0800

    Initial commit

---
+++ src/main/java/cn/edu/jmu/sqlonlinejudge/SqlonlinejudgeApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.jmu.sqlonlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlonlinejudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlonlinejudgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/jmu/sqlonlinejudge/SqlonlinejudgeApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.jmu.sqlonlinejudge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqlonlinejudgeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
