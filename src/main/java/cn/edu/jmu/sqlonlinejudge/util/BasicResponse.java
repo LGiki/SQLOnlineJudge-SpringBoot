8f7e4422544f04b29aa3403f0b5e03622ef47209
ShangguanHong <sgh1450280694@gmail.com>
2019-06-18 19:23:38 +0800

    新增基础响应对象

---
+++ src/main/java/cn/edu/jmu/sqlonlinejudge/util/BasicResponse.java
@@ -0,0 +1,34 @@
+package cn.edu.jmu.sqlonlinejudge.util;
+
+import lombok.Data;
+
+/**
+ * 基础响应对象，是应用所有的VO对象的父类
+ *
+ * @author sgh
+ * @date 2019/6/18 18:44
+ */
+@Data
+public class BasicResponse<T> {
+
+    /**
+     * 消息代码
+     * 0-200为正常响应，201-400为错误响应
+     */
+    private Integer code;
+
+    /**
+     * 消息内容
+     */
+    private String message;
+
+    /**
+     * 数据内容
+     */
+    private T data;
+
+    /**
+     * 异常信息
+     */
+    private String exception;
+}
