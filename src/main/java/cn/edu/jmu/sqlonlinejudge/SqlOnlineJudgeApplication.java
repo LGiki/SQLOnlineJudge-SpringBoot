e6175903726aaa6b21217169e2ae8c5e04a31323
LGiki <qq547221432@gmail.com>
2019-06-18 10:09:09 +0800

    1. Configure SpringBoot and MyBatis 2. Define project structure

--- src/main/java/cn/edu/jmu/sqlonlinejudge/SqlOnlineJudgeApplication.java
+++ src/main/java/cn/edu/jmu/sqlonlinejudge/SqlOnlineJudgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqlonlinejudgeApplication {
+public class SqlOnlineJudgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SqlonlinejudgeApplication.class, args);
+        SpringApplication.run(SqlOnlineJudgeApplication.class, args);
     }
 
 }
--- src/test/java/cn/edu/jmu/sqlonlinejudge/SqlOnlineJudgeApplicationTests.java
+++ src/test/java/cn/edu/jmu/sqlonlinejudge/SqlOnlineJudgeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SqlonlinejudgeApplicationTests {
+public class SqlOnlineJudgeApplicationTests {
 
     @Test
     public void contextLoads() {
