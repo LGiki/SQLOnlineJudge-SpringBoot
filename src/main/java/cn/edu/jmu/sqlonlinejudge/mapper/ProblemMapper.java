f5ef2310f7ffc8a469a9ff80f4bc0e44bf5e3530
LGiki <qq547221432@gmail.com>
2019-06-21 13:32:12 +0800

    Add problem model, mapper, service and service implement

---
+++ src/main/java/cn/edu/jmu/sqlonlinejudge/mapper/ProblemMapper.java
@@ -0,0 +1,58 @@
+package cn.edu.jmu.sqlonlinejudge.mapper;
+
+import cn.edu.jmu.sqlonlinejudge.model.Problem;
+
+/**
+ * @author LGiki
+ * @data 2019/06/21 13:19
+ */
+
+public interface ProblemMapper {
+    /**
+     * 通过ID删除题目
+     *
+     * @param id 题目ID
+     * @return 删除成功的记录条数
+     */
+    int deleteById(Integer id);
+
+    /**
+     * 添加题目
+     *
+     * @param record 题目对象
+     * @return 添加成功的记录条数
+     */
+    int insert(Problem record);
+
+    /**
+     * 选择性添加题目
+     *
+     * @param record 题目对象
+     * @return 添加成功的记录条数
+     */
+    int insertSelective(Problem record);
+
+    /**
+     * 通过ID获取题目信息
+     *
+     * @param id 题目ID
+     * @return 题目对象
+     */
+    Problem selectById(Integer id);
+
+    /**
+     * 选择性通过ID更新题目
+     *
+     * @param record 题目对象
+     * @return 更新成功的记录条数
+     */
+    int updateByIdSelective(Problem record);
+
+    /**
+     * 通过ID更新题目
+     *
+     * @param record 题目对象
+     * @return 更新成功的记录条数
+     */
+    int updateById(Problem record);
+}
---
+++ src/main/java/cn/edu/jmu/sqlonlinejudge/serviceimpl/ProblemServiceImpl.java
@@ -0,0 +1,50 @@
+package cn.edu.jmu.sqlonlinejudge.serviceimpl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import cn.edu.jmu.sqlonlinejudge.mapper.ProblemMapper;
+import cn.edu.jmu.sqlonlinejudge.model.Problem;
+import cn.edu.jmu.sqlonlinejudge.service.ProblemService;
+
+/**
+ * @author LGiki
+ * @date 2019/06/21 13:19
+ */
+
+@Service
+public class ProblemServiceImpl implements ProblemService{
+
+    @Resource
+    private ProblemMapper problemMapper;
+
+    @Override
+    public int deleteById(Integer id) {
+        return problemMapper.deleteById(id);
+    }
+
+    @Override
+    public int insert(Problem record) {
+        return problemMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(Problem record) {
+        return problemMapper.insertSelective(record);
+    }
+
+    @Override
+    public Problem selectById(Integer id) {
+        return problemMapper.selectById(id);
+    }
+
+    @Override
+    public int updateByIdSelective(Problem record) {
+        return problemMapper.updateByIdSelective(record);
+    }
+
+    @Override
+    public int updateById(Problem record) {
+        return problemMapper.updateById(record);
+    }
+
+}
