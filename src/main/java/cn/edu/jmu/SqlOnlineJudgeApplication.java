09e0c32aec6aab6c8a5968da2f918e03e4ffb22f
ShangguanHong <sgh1450280694@gmail.com>
2019-08-26 10:15:22 +0800

    解决跨域问题

--- src/main/java/cn/edu/jmu/SqlOnlineJudgeApplication.java
+++ src/main/java/cn/edu/jmu/SqlOnlineJudgeApplication.java
@@ -1,12 +1,19 @@
 package cn.edu.jmu;
 
+import cn.edu.jmu.security.config.ShiroConfig;
+import cn.edu.jmu.security.filter.CrossDomainFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-//@ServletComponentScan("cn.edu.jmu.security.filter")
+/**
+ * @author lgiki
+ */
+// @ServletComponentScan("cn.edu.jmu.security.filter")
 @SpringBootApplication
 @MapperScan("cn.edu.jmu.sqlonlinejudge.mapper")
+@Import({ShiroConfig.class, CrossDomainFilter.class})
 public class SqlOnlineJudgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SqlOnlineJudgeApplication.class, args);
