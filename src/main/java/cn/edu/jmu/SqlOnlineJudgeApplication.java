7e3eede7d3ebd06606d3d9487165262b0c3ebaa5
ShangguanHong <sgh1450280694@gmail.com>
2019-09-06 12:36:49 +0800

    feat: add call to python statement module

--- src/main/java/cn/edu/jmu/SqlOnlineJudgeApplication.java
+++ src/main/java/cn/edu/jmu/SqlOnlineJudgeApplication.java
@@ -1,6 +1,10 @@
 package cn.edu.jmu;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
+import org.python.core.PyFunction;
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +12,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lgiki
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("cn.edu.jmu.sqlonlinejudge.mapper")
 public class SqlOnlineJudgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SqlOnlineJudgeApplication.class, args);
     }
+
+    public static void test(String[] args) {
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.execfile("");
+        PyFunction func = interpreter.get("test2", PyFunction.class);
+        PyObject pyobj = func.__call__();
+        System.out.println("answer = " + pyobj.toString());
+    }
 }
+
