99e606d2027639212177ad78ba1baf576b79661b
ShangguanHong <sgh1450280694@gmail.com>
2019-10-08 14:55:35 +0800

    更新数据库文件

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : localhost_mysql
  Source Server Type    : MySQL
- Source Server Version : 50553
+ Source Server Version : 80012
  Source Host           : localhost:3306
  Source Schema         : sqlonlinejudge
 
  Target Server Type    : MySQL
- Target Server Version : 50553
+ Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 08/10/2019 10:56:50
+ Date: 08/10/2019 11:12:31
 */
 
 SET NAMES utf8mb4;
@@ -21,137 +21,262 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for data_base
 -- ----------------------------
 DROP TABLE IF EXISTS `data_base`;
-CREATE TABLE `data_base`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '数据库ID',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '数据库名称',
-  `create_table` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '建表语句',
-  `test_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '数据插入语句',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `data_base`
+(
+    `id`           int(11)                                                      NOT NULL AUTO_INCREMENT COMMENT '数据库ID',
+    `name`         varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '数据库名称',
+    `create_table` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci        NULL COMMENT '建表语句',
+    `test_data`    text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci        NULL COMMENT '数据插入语句',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of data_base
+-- ----------------------------
+INSERT INTO `data_base`
+VALUES (1, '1',
+        'create table t_coffee (\r\n    id INTEGER  PRIMARY KEY autoincrement,\r\n    name varchar(255),\r\n    price int\r\n);\r\n\r\n',
+        'insert into t_coffee (name, price) values (\'espresso\', 2000);\r\ninsert into t_coffee (name, price) values (\'latte\', 2500);\r\ninsert into t_coffee (name, price) values (\'capuccino\', 2500);\r\ninsert into t_coffee (name, price) values (\'mocha\', 3000);\r\ninsert into t_coffee (name, price) values (\'macchiato\', 3000);');
 
 -- ----------------------------
 -- Table structure for problem
 -- ----------------------------
 DROP TABLE IF EXISTS `problem`;
-CREATE TABLE `problem`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '题目ID',
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '题目标题',
-  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '题目描述',
-  `sample_output` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '样例输出',
-  `hint` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '提示',
-  `answer` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '答案',
-  `solve` int(11) NOT NULL DEFAULT 0 COMMENT '通过数',
-  `submit` int(11) NOT NULL DEFAULT 0 COMMENT '提交数',
-  `true_result` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '正确输出',
-  `database_id` int(11) NOT NULL DEFAULT 0 COMMENT '数据库ID',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FK_problem_database`(`database_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `problem`
+(
+    `id`            int(11)                                                NOT NULL AUTO_INCREMENT COMMENT '题目ID',
+    `title`         varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '题目标题',
+    `description`   text CHARACTER SET utf8 COLLATE utf8_general_ci        NOT NULL COMMENT '题目描述',
+    `sample_output` text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '样例输出',
+    `hint`          text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '提示',
+    `answer`        text CHARACTER SET utf8 COLLATE utf8_general_ci        NOT NULL COMMENT '答案',
+    `solve`         int(11)                                                NOT NULL DEFAULT 0 COMMENT '通过数',
+    `submit`        int(11)                                                NOT NULL DEFAULT 0 COMMENT '提交数',
+    `true_result`   text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '正确输出',
+    `database_id`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '数据库ID',
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `FK_problem_database` (`database_id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of problem
+-- ----------------------------
+INSERT INTO `problem`
+VALUES (1, '1', '1', '1', '1', 'select * from t_coffee', 0, 0, NULL, 1);
+INSERT INTO `problem`
+VALUES (2, '2', '2', '2', '2', '2', 0, 0, NULL, 1);
 
 -- ----------------------------
 -- Table structure for solution
 -- ----------------------------
 DROP TABLE IF EXISTS `solution`;
-CREATE TABLE `solution`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '解答ID',
-  `uid` int(11) NULL DEFAULT NULL COMMENT '用户ID',
-  `pid` int(11) NULL DEFAULT NULL COMMENT '题目ID',
-  `source_code` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '源代码',
-  `submit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '提交时间',
-  `run_error` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '错误信息',
-  `result` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '结果',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `uid`(`uid`) USING BTREE,
-  INDEX `pid`(`pid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `solution`
+(
+    `id`          int(11)                                               NOT NULL AUTO_INCREMENT COMMENT '解答ID',
+    `uid`         int(11)                                               NULL     DEFAULT NULL COMMENT '用户ID',
+    `pid`         int(11)                                               NULL     DEFAULT NULL COMMENT '题目ID',
+    `source_code` text CHARACTER SET utf8 COLLATE utf8_general_ci       NOT NULL COMMENT '源代码',
+    `submit_time` timestamp(0)                                          NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '提交时间',
+    `run_error`   text CHARACTER SET utf8 COLLATE utf8_general_ci       NULL COMMENT '错误信息',
+    `result`      varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '结果',
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `uid` (`uid`) USING BTREE,
+    INDEX `pid` (`pid`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of solution
+-- ----------------------------
+INSERT INTO `solution`
+VALUES (1, 1, 1, 'select * from t_coffee', '2011-06-24 18:37:39', '', '0');
+INSERT INTO `solution`
+VALUES (2, 1, 1, '2', '2019-08-26 21:46:51', NULL, '1');
+INSERT INTO `solution`
+VALUES (3, 2, 2, '3', '2019-08-26 13:47:45', NULL, '1');
 
 -- ----------------------------
 -- Table structure for sys_admin
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_admin`;
-CREATE TABLE `sys_admin`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `salt` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_admin`
+(
+    `id`       int(11)                                                      NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `salt`     varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `username` (`username`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_admin
+-- ----------------------------
+INSERT INTO `sys_admin`
+VALUES (1, 'admin', '8315af46b6d38ebdaec05ec05393edd3', 'NOOQnVyN2NI2GD4q/V28');
+INSERT INTO `sys_admin`
+VALUES (2, 'a', 'a', 'a');
+INSERT INTO `sys_admin`
+VALUES (3, 'sgh', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt');
 
 -- ----------------------------
 -- Table structure for sys_admin_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_admin_role`;
-CREATE TABLE `sys_admin_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `a_id` int(11) NOT NULL DEFAULT 0,
-  `r_id` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_admin_role`
+(
+    `id`   int(11) NOT NULL AUTO_INCREMENT,
+    `a_id` int(11) NOT NULL DEFAULT 0,
+    `r_id` int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_admin_role
+-- ----------------------------
+INSERT INTO `sys_admin_role`
+VALUES (1, 1, 1);
+INSERT INTO `sys_admin_role`
+VALUES (2, 3, 1);
+INSERT INTO `sys_admin_role`
+VALUES (3, 4, 2);
 
 -- ----------------------------
 -- Table structure for sys_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE `sys_permission`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  `permission` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_permission`
+(
+    `id`         int(11)                                                      NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+    `permission` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission`
+VALUES (1, '总权限', '*:*');
 
 -- ----------------------------
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  `description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `role_name`(`role_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_role`
+(
+    `id`          int(11)                                                      NOT NULL AUTO_INCREMENT,
+    `role_name`   varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+    `description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `role_name` (`role_name`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role`
+VALUES (1, 'admin', '总管理员');
 
 -- ----------------------------
 -- Table structure for sys_role_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_permission`;
-CREATE TABLE `sys_role_permission`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `r_id` int(11) NOT NULL DEFAULT 0,
-  `p_id` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_role_permission`
+(
+    `id`   int(11) NOT NULL AUTO_INCREMENT,
+    `r_id` int(11) NOT NULL DEFAULT 0,
+    `p_id` int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_role_permission
+-- ----------------------------
+INSERT INTO `sys_role_permission`
+VALUES (1, 1, 1);
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '用户名',
-  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱地址',
-  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-  `salt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码盐',
-  `submit` int(11) NOT NULL DEFAULT 0 COMMENT '提交数',
-  `solved` int(11) NOT NULL DEFAULT 0 COMMENT '通过数',
-  `avatar` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'default.jpg' COMMENT '头像',
-  `status` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '状态,0未激活，1正常，2锁定',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `username`(`username`) USING BTREE,
-  UNIQUE INDEX `email`(`email`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `sys_user`
+(
+    `id`       int(11)                                                NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+    `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '用户名',
+    `email`    varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱地址',
+    `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+    `salt`     varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码盐',
+    `submit`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '提交数',
+    `solved`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '通过数',
+    `avatar`   varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT 'default.jpg' COMMENT '头像',
+    `status`   varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '0' COMMENT '状态,0未激活，1正常，2锁定',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `username` (`username`) USING BTREE,
+    UNIQUE INDEX `email` (`email`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 13
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user`
+VALUES (1, '1', '1', '1', '', 100, 5, 'default.jpg', '1');
+INSERT INTO `sys_user`
+VALUES (2, '2', '2', '2', '2', 5555, 6, 'default.jpg', '1');
+INSERT INTO `sys_user`
+VALUES (7, 'sgh', 'sgh@qq.com', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt', 0, 7, 'default.jpg', '1');
+INSERT INTO `sys_user`
+VALUES (10, '5555', '5555@qq.com', '24caf03e132a9a1d31dad6e19b7cb723', 'DpZtEwvvBwpPLNKWAccS', 0, 0, 'default.jpg',
+        '0');
 
 -- ----------------------------
 -- Table structure for user_problem
 -- ----------------------------
 DROP TABLE IF EXISTS `user_problem`;
-CREATE TABLE `user_problem`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户题库ID',
-  `uid` int(11) NULL DEFAULT NULL COMMENT '用户ID',
-  `pid` int(11) NULL DEFAULT NULL COMMENT '题目ID',
-  `state` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户题目状态',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `user_problem`
+(
+    `id`    int(11)                                                     NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `uid`   int(11)                                                     NULL DEFAULT NULL COMMENT '用户id',
+    `pid`   int(11)                                                     NULL DEFAULT NULL COMMENT '题目id',
+    `state` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
