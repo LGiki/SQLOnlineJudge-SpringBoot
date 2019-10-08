ba4ffb8bc218b1da39d5db4c636b14dd00a364f4
ShangguanHong <sgh1450280694@gmail.com>
2019-10-08 19:19:01 +0800

    更新数据库文件

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 08/10/2019 11:12:31
+ Date: 08/10/2019 19:18:32
 */
 
 SET NAMES utf8mb4;
@@ -38,9 +38,13 @@ CREATE TABLE `data_base`
 -- Records of data_base
 -- ----------------------------
 INSERT INTO `data_base`
-VALUES (1, '1',
-        'create table t_coffee (\r\n    id INTEGER  PRIMARY KEY autoincrement,\r\n    name varchar(255),\r\n    price int\r\n);\r\n\r\n',
-        'insert into t_coffee (name, price) values (\'espresso\', 2000);\r\ninsert into t_coffee (name, price) values (\'latte\', 2500);\r\ninsert into t_coffee (name, price) values (\'capuccino\', 2500);\r\ninsert into t_coffee (name, price) values (\'mocha\', 3000);\r\ninsert into t_coffee (name, price) values (\'macchiato\', 3000);');
+VALUES (4, '图书馆',
+        'CREATE TABLE `employees` (\n`emp_no` int(11) NOT NULL,\n`birth_date` date NOT NULL,\n`first_name` varchar(14) NOT NULL,\n`last_name` varchar(16) NOT NULL,\n`gender` char(1) NOT NULL,\n`hire_date` date NOT NULL,\nPRIMARY KEY (`emp_no`));',
+        'INSERT INTO employees VALUES(10001,\'1953-09-02\',\'Georgi\',\'Facello\',\'M\',\'1986-06-26\'); INSERT INTO employees VALUES(10002,\'1964-06-02\',\'Bezalel\',\'Simmel\',\'F\',\'1985-11-21\'); INSERT INTO employees VALUES(10003,\'1959-12-03\',\'Parto\',\'Bamford\',\'M\',\'1986-08-28\'); INSERT INTO employees VALUES(10004,\'1954-05-01\',\'Chirstian\',\'Koblick\',\'M\',\'1986-12-01\'); INSERT INTO employees VALUES(10005,\'1955-01-21\',\'Kyoichi\',\'Maliniak\',\'M\',\'1989-09-12\'); INSERT INTO employees VALUES(10006,\'1953-04-20\',\'Anneke\',\'Preusig\',\'F\',\'1989-06-02\'); INSERT INTO employees VALUES(10007,\'1957-05-23\',\'Tzvetan\',\'Zielinski\',\'F\',\'1989-02-10\'); INSERT INTO employees VALUES(10008,\'1958-02-19\',\'Saniya\',\'Kalloufi\',\'M\',\'1994-09-15\'); INSERT INTO employees VALUES(10009,\'1952-04-19\',\'Sumant\',\'Peac\',\'F\',\'1985-02-18\'); INSERT INTO employees VALUES(10010,\'1963-06-01\',\'Duangkaew\',\'Piveteau\',\'F\',\'1989-08-24\'); INSERT INTO employees VALUES(10011,\'1953-11-07\',\'Mary\',\'Sluis\',\'F\',\'1990-01-22\');');
+INSERT INTO `data_base`
+VALUES (9, '图书馆2',
+        'CREATE TABLE `employees` (\n`emp_no` int(11) NOT NULL,\n`birth_date` date NOT NULL,\n`first_name` varchar(14) NOT NULL,\n`last_name` varchar(16) NOT NULL,\n`gender` char(1) NOT NULL,\n`hire_date` date NOT NULL,\nPRIMARY KEY (`emp_no`));',
+        'INSERT INTO employees VALUES(10001,\'1953-09-02\',\'Georgi\',\'Facello\',\'M\',\'1986-06-26\'); INSERT INTO employees VALUES(10002,\'1964-06-02\',\'Bezalel\',\'Simmel\',\'F\',\'1985-11-21\'); INSERT INTO employees VALUES(10003,\'1959-12-03\',\'Parto\',\'Bamford\',\'M\',\'1986-08-28\'); INSERT INTO employees VALUES(10004,\'1954-05-01\',\'Chirstian\',\'Koblick\',\'M\',\'1986-12-01\'); INSERT INTO employees VALUES(10005,\'1955-01-21\',\'Kyoichi\',\'Maliniak\',\'M\',\'1989-09-12\'); INSERT INTO employees VALUES(10006,\'1953-04-20\',\'Anneke\',\'Preusig\',\'F\',\'1989-06-02\'); INSERT INTO employees VALUES(10007,\'1957-05-23\',\'Tzvetan\',\'Zielinski\',\'F\',\'1989-02-10\'); INSERT INTO employees VALUES(10008,\'1958-02-19\',\'Saniya\',\'Kalloufi\',\'M\',\'1994-09-15\'); INSERT INTO employees VALUES(10009,\'1952-04-19\',\'Sumant\',\'Peac\',\'F\',\'1985-02-18\'); INSERT INTO employees VALUES(10010,\'1963-06-01\',\'Duangkaew\',\'Piveteau\',\'F\',\'1989-08-24\'); INSERT INTO employees VALUES(10011,\'1953-11-07\',\'Mary\',\'Sluis\',\'F\',\'1990-01-22\');');
 
 -- ----------------------------
 -- Table structure for problem
@@ -48,18 +52,19 @@ VALUES (1, '1',
 DROP TABLE IF EXISTS `problem`;
 CREATE TABLE `problem`
 (
-    `id`            int(11)                                                NOT NULL AUTO_INCREMENT COMMENT '题目ID',
-    `title`         varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '题目标题',
-    `description`   text CHARACTER SET utf8 COLLATE utf8_general_ci        NOT NULL COMMENT '题目描述',
-    `sample_output` text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '样例输出',
-    `hint`          text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '提示',
-    `answer`        text CHARACTER SET utf8 COLLATE utf8_general_ci        NOT NULL COMMENT '答案',
-    `solve`         int(11)                                                NOT NULL DEFAULT 0 COMMENT '通过数',
-    `submit`        int(11)                                                NOT NULL DEFAULT 0 COMMENT '提交数',
-    `true_result`   text CHARACTER SET utf8 COLLATE utf8_general_ci        NULL COMMENT '正确输出',
-    `database_id`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '数据库ID',
+    `id`            int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '题目ID',
+    `title`         varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '题目标题',
+    `description`   text CHARACTER SET utf8 COLLATE utf8_general_ci         NOT NULL COMMENT '题目描述',
+    `sample_output` text CHARACTER SET utf8 COLLATE utf8_general_ci         NULL COMMENT '样例输出',
+    `hint`          text CHARACTER SET utf8 COLLATE utf8_general_ci         NULL COMMENT '提示',
+    `answer`        text CHARACTER SET utf8 COLLATE utf8_general_ci         NOT NULL COMMENT '答案',
+    `solve`         int(11)                                                 NOT NULL DEFAULT 0 COMMENT '通过数',
+    `submit`        int(11)                                                 NOT NULL DEFAULT 0 COMMENT '提交数',
+    `true_result`   text CHARACTER SET utf8 COLLATE utf8_general_ci         NULL COMMENT '正确输出',
+    `database_id`   int(11)                                                 NOT NULL DEFAULT 0 COMMENT '数据库ID',
     PRIMARY KEY (`id`) USING BTREE,
-    INDEX `FK_problem_database` (`database_id`) USING BTREE
+    INDEX `FK_problem_database` (`database_id`) USING BTREE,
+    CONSTRAINT `FK_problem_data_base` FOREIGN KEY (`database_id`) REFERENCES `data_base` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB
   CHARACTER SET = utf8
   COLLATE = utf8_general_ci
@@ -69,9 +74,15 @@ CREATE TABLE `problem`
 -- Records of problem
 -- ----------------------------
 INSERT INTO `problem`
-VALUES (1, '1', '1', '1', '1', 'select * from t_coffee', 0, 0, NULL, 1);
+VALUES (3, '查找入职员工时间排名倒数第三的员工所有信息', '查找入职员工时间排名倒数第三的员工所有信息', '', '',
+        'select * from employees order by hire_date desc limit 3', 0, 0,
+        '[(10008, \'1958-02-19\', \'Saniya\', \'Kalloufi\', \'M\', \'1994-09-15\'), (10011, \'1953-11-07\', \'Mary\', \'Sluis\', \'F\', \'1990-01-22\'), (10005, \'1955-01-21\', \'Kyoichi\', \'Maliniak\', \'M\', \'1989-09-12\')]',
+        4);
 INSERT INTO `problem`
-VALUES (2, '2', '2', '2', '2', '2', 0, 0, NULL, 1);
+VALUES (6, '查找所有员工的last_name和first_name以及对应部门编号dept_no，也包括展示没有分配具体部门的员工',
+        '查找所有员工的last_name和first_name以及对应部门编号dept_no，也包括展示没有分配具体部门的员工', '', '', 'select * from employees', 0, 0,
+        '[(10001, \'1953-09-02\', \'Georgi\', \'Facello\', \'M\', \'1986-06-26\'), (10002, \'1964-06-02\', \'Bezalel\', \'Simmel\', \'F\', \'1985-11-21\'), (10003, \'1959-12-03\', \'Parto\', \'Bamford\', \'M\', \'1986-08-28\'), (10004, \'1954-05-01\', \'Chirstian\', \'Koblick\', \'M\', \'1986-12-01\'), (10005, \'1955-01-21\', \'Kyoichi\', \'Maliniak\', \'M\', \'1989-09-12\'), (10006, \'1953-04-20\', \'Anneke\', \'Preusig\', \'F\', \'1989-06-02\'), (10007, \'1957-05-23\', \'Tzvetan\', \'Zielinski\', \'F\', \'1989-02-10\'), (10008, \'1958-02-19\', \'Saniya\', \'Kalloufi\', \'M\', \'1994-09-15\'), (10009, \'1952-04-19\', \'Sumant\', \'Peac\', \'F\', \'1985-02-18\'), (10010, \'1963-06-01\', \'Duangkaew\', \'Piveteau\', \'F\', \'1989-08-24\'), (10011, \'1953-11-07\', \'Mary\', \'Sluis\', \'F\', \'1990-01-22\')]',
+        4);
 
 -- ----------------------------
 -- Table structure for solution
@@ -88,7 +99,8 @@ CREATE TABLE `solution`
     `result`      varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '结果',
     PRIMARY KEY (`id`) USING BTREE,
     INDEX `uid` (`uid`) USING BTREE,
-    INDEX `pid` (`pid`) USING BTREE
+    INDEX `pid` (`pid`) USING BTREE,
+    CONSTRAINT `FK_solution_problem` FOREIGN KEY (`pid`) REFERENCES `problem` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 3
   CHARACTER SET = utf8
@@ -99,11 +111,17 @@ CREATE TABLE `solution`
 -- Records of solution
 -- ----------------------------
 INSERT INTO `solution`
-VALUES (1, 1, 1, 'select * from t_coffee', '2011-06-24 18:37:39', '', '0');
+VALUES (4, 7, 3, 'select * from employees order by hire_date desc limit 3', '2019-10-08 15:23:15', NULL, '1');
 INSERT INTO `solution`
-VALUES (2, 1, 1, '2', '2019-08-26 21:46:51', NULL, '1');
+VALUES (5, 7, 3, 'select * from employees', '2019-10-08 15:27:02', '123', '3');
 INSERT INTO `solution`
-VALUES (3, 2, 2, '3', '2019-08-26 13:47:45', NULL, '1');
+VALUES (8, 14, 3, 'select * from employees order by hire_date desc limit 3', '2019-10-08 15:48:45', NULL, '1');
+INSERT INTO `solution`
+VALUES (9, 14, 3, 'select * from employees order by hire_date desc', '2019-10-08 15:49:09', NULL, '3');
+INSERT INTO `solution`
+VALUES (10, 14, 3, 'select * from employees order by hire_date desc limit 3', '2019-10-08 15:49:30', NULL, '1');
+INSERT INTO `solution`
+VALUES (11, 14, 6, 'select * FROM employees', '2019-10-08 15:52:17', NULL, '1');
 
 -- ----------------------------
 -- Table structure for sys_admin
@@ -115,6 +133,7 @@ CREATE TABLE `sys_admin`
     `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     `salt`     varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `status`   bit(1)                                                       NOT NULL DEFAULT b'1' COMMENT '状态,0锁定，1正常',
     PRIMARY KEY (`id`) USING BTREE,
     UNIQUE INDEX `username` (`username`) USING BTREE
 ) ENGINE = InnoDB
@@ -127,11 +146,11 @@ CREATE TABLE `sys_admin`
 -- Records of sys_admin
 -- ----------------------------
 INSERT INTO `sys_admin`
-VALUES (1, 'admin', '8315af46b6d38ebdaec05ec05393edd3', 'NOOQnVyN2NI2GD4q/V28');
+VALUES (1, 'admin', '8315af46b6d38ebdaec05ec05393edd3', 'NOOQnVyN2NI2GD4q/V28', b'1');
 INSERT INTO `sys_admin`
-VALUES (2, 'a', 'a', 'a');
+VALUES (2, 'a', 'a', 'a', b'1');
 INSERT INTO `sys_admin`
-VALUES (3, 'sgh', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt');
+VALUES (3, 'sgh', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt', b'1');
 
 -- ----------------------------
 -- Table structure for sys_admin_role
@@ -240,7 +259,7 @@ CREATE TABLE `sys_user`
     `submit`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '提交数',
     `solved`   int(11)                                                NOT NULL DEFAULT 0 COMMENT '通过数',
     `avatar`   varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT 'default.jpg' COMMENT '头像',
-    `status`   varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '0' COMMENT '状态,0未激活，1正常，2锁定',
+    `status`   bit(1)                                                 NOT NULL DEFAULT b'1' COMMENT '状态,0锁定，1正常',
     PRIMARY KEY (`id`) USING BTREE,
     UNIQUE INDEX `username` (`username`) USING BTREE,
     UNIQUE INDEX `email` (`email`) USING BTREE
@@ -254,14 +273,16 @@ CREATE TABLE `sys_user`
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user`
-VALUES (1, '1', '1', '1', '', 100, 5, 'default.jpg', '1');
+VALUES (1, '1', '1', '1', '', 100, 5, 'default.jpg', b'1');
 INSERT INTO `sys_user`
-VALUES (2, '2', '2', '2', '2', 5555, 6, 'default.jpg', '1');
+VALUES (2, '2', '2', '2', '2', 5555, 6, 'default.jpg', b'1');
 INSERT INTO `sys_user`
-VALUES (7, 'sgh', 'sgh@qq.com', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt', 0, 7, 'default.jpg', '1');
+VALUES (7, 'sgh', 'sgh@qq.com', '7078553a2476e9c86ea74234145febef', 'yK5o/VZdWdpJplm+5VVt', 3, 8, 'default.jpg', b'1');
 INSERT INTO `sys_user`
 VALUES (10, '5555', '5555@qq.com', '24caf03e132a9a1d31dad6e19b7cb723', 'DpZtEwvvBwpPLNKWAccS', 0, 0, 'default.jpg',
-        '0');
+        b'0');
+INSERT INTO `sys_user`
+VALUES (14, 'ssss', 'ss@qq.com', '7a222d20522d354bde7526e58b3798a4', 'D1xACs3Dsl16eJqRbYrF', 4, 2, 'default.jpg', b'1');
 
 -- ----------------------------
 -- Table structure for user_problem
@@ -269,14 +290,24 @@ VALUES (10, '5555', '5555@qq.com', '24caf03e132a9a1d31dad6e19b7cb723', 'DpZtEwvv
 DROP TABLE IF EXISTS `user_problem`;
 CREATE TABLE `user_problem`
 (
-    `id`    int(11)                                                     NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `uid`   int(11)                                                     NULL DEFAULT NULL COMMENT '用户id',
-    `pid`   int(11)                                                     NULL DEFAULT NULL COMMENT '题目id',
-    `state` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态',
+    `id`    int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `uid`   int(11) NULL DEFAULT NULL COMMENT '用户id',
+    `pid`   int(11) NULL DEFAULT NULL COMMENT '题目id',
+    `state` bit(1)  NULL DEFAULT NULL COMMENT '状态,1表示通过,0表示尝试但未通过',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
   COLLATE = utf8mb4_general_ci
   ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of user_problem
+-- ----------------------------
+INSERT INTO `user_problem`
+VALUES (1, 7, 3, b'1');
+INSERT INTO `user_problem`
+VALUES (2, 14, 3, b'1');
+INSERT INTO `user_problem`
+VALUES (3, 14, 6, b'1');
 
 SET FOREIGN_KEY_CHECKS = 1;
