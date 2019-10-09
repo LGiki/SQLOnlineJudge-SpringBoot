2cdb47c8a3244ab507134ec85451bc7e293d3399
LGiki <qq547221432@gmail.com>
2019-10-09 09:35:18 +0800

    Remove default username and password in backend login page

--- backend/src/views/login/index.vue
+++ backend/src/views/login/index.vue
@@ -74,8 +74,8 @@ export default {
     // }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'admin'
+        username: '',
+        password: ''
       },
       loginRules: {
         // username: [{ required: true, trigger: 'blur', validator: validateUsername }],
