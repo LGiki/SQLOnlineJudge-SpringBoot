862c09ea446791df8aecdfddca89410a75e21eeb
LGiki <qq547221432@gmail.com>
2019-10-10 12:25:10 +0800

    Fix bugs

--- judger/getTrueResultByProblemAnswer.py
+++ judger/getTrueResultByProblemAnswer.py
@@ -63,8 +63,6 @@ def exec_code(sqlite_db_file_path, source_code):
 # 通过问题ID获取正确答案
 def get_true_result(SQLITE_DIR, SQLITE_TEMP_DIR, answer, database_id):
     sqlite_db_file_path = os.path.join(SQLITE_DIR, '{}.db'.format(database_id))
-    if not os.path.exists(sqlite_db_file_path):
-        return construct_json_response(RESPONSE_CODE['NO_DB_FILE'], None, '无法找到该题目对应数据库文件')
     temp_sqlite_db_file_path = os.path.join(SQLITE_TEMP_DIR, '{}_{}_temp.db'.format(database_id, uuid.uuid4()))
     shutil.copyfile(sqlite_db_file_path, temp_sqlite_db_file_path)
     true_result, run_exception = exec_code(sqlite_db_file_path, answer)
@@ -87,6 +85,9 @@ def main(answer, database_id):
         return construct_json_response(RESPONSE_CODE['FAIL'], None, 'Can not load config.ini.')
     SQLITE_DIR, SQLITE_TEMP_DIR, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_CHARSET = init_config(config_parser)
     init_work_directory(SQLITE_TEMP_DIR)
+    sqlite_db_file_path = os.path.join(SQLITE_DIR, '{}.db'.format(database_id))
+    if not os.path.exists(sqlite_db_file_path):
+        return construct_json_response(RESPONSE_CODE['NO_DB_FILE'], None, '无法找到该题目对应数据库文件')
     true_result, run_exception = get_true_result(SQLITE_DIR, SQLITE_TEMP_DIR, answer, database_id)
     if true_result is None:
         return construct_json_response(RESPONSE_CODE['FAIL'], None, run_exception)
